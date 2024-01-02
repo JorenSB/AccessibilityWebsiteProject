db2f994fdebc6ab33f59ecbfab2e7080c2902ab9
Youri <youriknoop.stage>
2024-01-02 16:17:05 +0100

    Please just Push

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,11 +1,10 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+import Example from "./components/layouts/Example";
+
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Example />
   },
 ];
 
