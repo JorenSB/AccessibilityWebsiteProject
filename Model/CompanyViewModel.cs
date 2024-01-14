cca190f8b8b7411b728dacfc7a3692aeda073026
Joren <Jorenbanens@hotmail.com>
2024-01-14 03:05:41 +0100

    Added 2 passwords in preparation password update Added AddressViewModel for security to front-end

--- Model/CompanyViewModel.cs
+++ Model/CompanyViewModel.cs
@@ -1,11 +1,11 @@
 public class CompanyViewModel
 {
     public string? Information { get; set; }
-
-    public string? CompanyName {get; set;}
-    public string? Password { get; set; }
-    public string? Email { get; set;}
+    public string? CompanyName { get; set; }
+    public string? CurrentPassword { get; set; }
+    public string? NewPassword { get; set; }
+    public string? Email { get; set; }
     public string? Url { get; set; }
     public string? KvkNumber { get; set; }
-    public Address? Address { get; set; }
+    public AddressViewModel? Address { get; set; }
 }
