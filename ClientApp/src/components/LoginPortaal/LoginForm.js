97a25df9cf09112bf008cb42cdc1de0f4c64a495
Joren <Jorenbanens@hotmail.com>
2024-01-16 19:05:42 +0100

    Added Company and Admin routes

--- ClientApp/src/components/LoginPortaal/LoginForm.js
+++ ClientApp/src/components/LoginPortaal/LoginForm.js
@@ -37,7 +37,9 @@ function LoginForm() {
                     if (role === "Expert") {
                         navigate("/deskundige");
                     } else if (role === "Company") {
-                        navigate("/test");
+                        navigate("/company");
+                    } else if (role === "Admin") {
+                        navigate("/admin")
                     } else {
                         console.log("mislukt tijdens het inloggen", "role = " + role);
                     }
@@ -59,10 +61,12 @@ function LoginForm() {
           if (isAuthenticated()) {
             const role = getUserRole();
       
-            if (role === 'Expert') {
-              navigate('/deskundige');
-            } else if (role === 'Company') {
-              navigate('/test');
+            if (role === "Expert") {
+                navigate("/deskundige");
+            } else if (role === "Company") {
+                navigate("/company");
+            } else if (role === "Admin") {
+                navigate("/admin")
             }
           }
         };
