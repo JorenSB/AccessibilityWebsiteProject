d9d63bf38eb7f1ecfb58542c5f1864e76755b968
Joren <Jorenbanens@hotmail.com>
2024-01-17 15:48:26 +0100

    fixed typo

--- ClientApp/src/components/layouts/LogoutButton.js
+++ ClientApp/src/components/layouts/LogoutButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-const LogOutButton = () => {
+const LogoutButton = () => {
 
   const navigate = useNavigate();
     async function logout(){
@@ -41,4 +41,4 @@ const LogOutButton = () => {
   );
 };
 
-export default LogOutButton;
+export default LogoutButton;
