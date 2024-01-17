c9acf0c3b676b59d539ef06591f0ea05ff355fce
Joren <Jorenbanens@hotmail.com>
2024-01-17 17:46:37 +0100

    Reworked api and models to work with new way of handling jwt tokens

--- Model/StudyViewModel.cs
+++ Model/StudyViewModel.cs
@@ -7,4 +7,19 @@ public class StudyViewModel
     public string Beperking { get; set; }
     public DateTime Date { get; set; }
     public ResultModelView? Result { get; set; }
+    public StudyViewModel()
+    {
+
+    }
+    public StudyViewModel(Study study)
+    {
+        Title = study.Title;
+        Status = study.Status;
+        Reward = study.Reward;
+        Language = study.Language;
+        Beperking = study.Beperking;
+        Date = study.Date;
+        if (study.Result != null)
+            Result = new ResultModelView(study.Result);
+    }
 }
